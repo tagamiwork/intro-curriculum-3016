fe0e81217b9bbf69c2b2f470f750d91f569ca40b
tagamiwork <uedawork2020@gmail.com>
2020-08-04 21:32:04 +0900

    日付のログを削除

--- index.js
+++ index.js
@@ -2,33 +2,31 @@
 const http = require('http');
 const pug = require('pug');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '焼き肉',
-          secondItem: 'しゃぶしゃぶ'
-        }));
+      let firstItem = `初期値_1`;
+      let secondItem = `初期値_2`;
+
+      if (req.url === '/enquetes/yaki-shabu') {  
+          firstItem = '焼き肉';
+          secondItem = 'しゃぶしゃぶ';
       } else if (req.url === '/enquetes/rice-bread') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: 'ごはん',
-          secondItem: 'パン'
-        }));
+          firstItem = 'ごはん';
+          secondItem = 'パン';
       } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '寿司',
-          secondItem: 'ピザ'
-        }));
+          firstItem = '寿司';
+          secondItem = 'ピザ';
       }
+      res.write(pug.renderFile('./form.pug', {
+        path: req.url,
+        firstItem: firstItem,
+        secondItem: firstItem
+      }));
       res.end();
       break;
     case 'POST':
